7f33af4027fbbaaba9598f768bfd301cf190390e
BugHunter-X <askzenai@gmail.com>
2025-01-28 11:46:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This bug occurs when using Tailwind CSS with a framework that uses a different CSS-in-JS solution. For example, if you are using Emotion or Styled-Components alongside Tailwind CSS, you might encounter unexpected styling issues or conflicts. The issue is that both solutions try to manage styles differently, causing unpredictable outcomes.
